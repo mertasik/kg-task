ee944955d6d31585f5fae1020836f89d3f5f4311
mert <mert1495@gmail.com>
2022-03-31 16:03:41 +0300

    scripts added

---
+++ config.py
@@ -0,0 +1,10 @@
+# SETTINGS and CONSTANTS    # Examples
+# [JIRA]
+JIRA_URL =                  # "http://44.202.185.140:8080/"
+JIRA_USER =                 # "admin"
+JIRA_PASS =                 # "123456"
+
+# [BITBUCKET]           # Examples
+BB_API_URL =                # "http://44.202.185.140:7990/"
+BB_USER =                   # "admin"
+BB_PASS =                   # "123456"
---
+++ jirabb.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import json
+import sys
+from Jira import Jira
+from BitBucket import BitBucket
+from config import *
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) != 3:
+        print("ERROR : Use as jirabb.py PK issueStatus ")
+        exit()
+    pk = args[1]
+    status = args[2]
+
+    jira = Jira(JIRA_URL, JIRA_USER, JIRA_PASS)
+    bb = BitBucket(BB_API_URL,
+                   BB_USER,BB_PASS)
+    jira.set_project(pk)
+    issues = jira.get_project_issues()
+    for issue in issues:
+        if str(issue.fields.status) == status:
+            
+            project_name = issue.fields.customfield_10100
+            repo_name = issue.fields.customfield_10101
+            
+            if bb.project_exists(project_name) is False:
+                project_k = bb.create_project_key(project_name)
+                project_key = bb.random_project_key(project_k)
+                p_data = {
+                    "key" : project_key,
+                    "name" : project_name
+                }
+                bb.create_project(json.dumps(p_data))
+                print("Project Created on BitBucket")
+                r_data ={
+                    "name" : repo_name
+                }
+                bb.create_repo(json.dumps(r_data), project_key)
+                print("Repository Created on BitBucket")
+                jira.jira.transition_issue(issue, 21)
+                print("Issue updated to Done on JIRA")
+            else:
+                print("Project Exists on BitBucket")
+                project_key = bb.get_project_key(project_name)
+                if bb.repo_exists(project_key, repo_name) is False:
+                    r_data ={
+                        "name" : repo_name
+                    }
+                    bb.create_repo(json.dumps(r_data), project_key)
+                    print("Repository Created on BitBucket")
+                    jira.jira.transition_issue(issue, 21)
+                    print("Issue updated to Done on JIRA")
